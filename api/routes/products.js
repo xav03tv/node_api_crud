52d59224c9e909990c60a46342463dc3241fad56
Xavier C <ch.xavier03@gmail.com>
2021-07-11 10:12:53 +0200

    test

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/products');
 
 router.get('/',(req, res, next)=>{
-    res.status(200).json({
-        message : 'Handling GET requests to /products'
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
     })
+    .catch( err => {
+        console.log(err)
+        res.status(500).json({
+            "message" : err
+        });
+    });
 })
 
 router.post('/',(req, res, next)=>{
-    const product = {
+    const product = new Product({
+        _id : new mongoose.Types.ObjectId(),
         name : req.body.name,
-        price : req.body.price,
-    }
+        price : req.body.price
+    })
+    product.save().then(result=>{
+        console.log(result)
+    })
+    .catch(error=>{
+        console.log(error)
+    })
     res.status(200).json({
         message : 'Handling POST requests to /products',
         createdProduct : product
@@ -20,16 +39,28 @@ router.post('/',(req, res, next)=>{
 
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId;
-    if(id === 'special'){
-        res.status(200).json({
-            message : 'Tu as découvert l id Special',
-            id : id
-        })
+    const ObjectID = mongoose.Types.ObjectId;
+    if(ObjectID.isValid(id)){
+        Product.findById(id)
+   .exec()
+   .then(doc => {
+       console.log("From data base : ",doc)
+       if(doc){
+        res.status(200).json(doc)
+       }else{
+           res.status(404).json({message : "L'id n'est pas valide"})
+       }
+       
+   })
+   .catch(err=>{
+       console.log(err);
+       res.status(500).json({error : error});
+    });
     }else{
-        res.status(200).json({
-            message : "Tu as passé un ID"
-        })
+        console.log("L'id n'est pas valide")
+        res.status(500).json({error : "L'id n'est pas valide"});
     }
+   
 })
 
 router.patch('/:productId', (req, res, next)=>{
--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser  =require("body-parser");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect("mongodb+srv://xav03:"+process.env.BDD_PASS+"@cluster0.lc5vk.mongodb.net/authentificator?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -19,7 +24,6 @@ app.use((req, res, next)=>{
     }
     next();
 });
-
 
 //Routes qui manipulent la requete
 app.use('/products', productRoutes);
