26cbe64de482183e575e6f9fda5850fa123cb929
Xavier C <ch.xavier03@gmail.com>
2021-07-12 00:00:17 +0200

    Ajout du systeme de commande

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,123 +1,146 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Product = require('../models/products');
+const Product = require("../models/products");
 
 //Récupere tous les produits
-router.get('/',(req, res, next)=>{
-    Product.find()
+router.get("/", (req, res, next) => {
+  Product.find()
     .select("name price _id")
     .exec()
-    .then(docs => {
-        const response = {
-            count : docs.length,
-            products : docs.map(doc=>{
-                return {
-                    name : doc.name,
-                    price : doc.price,
-                    _id : doc._id,
-                    request : {
-                        "type" : "GET",
-                        "url" : "http://localhost:3000/products/" + doc._id
-                    }
-                }
-            })
-        }
-        // if(docs.length >=0){
-            res.status(200).json(response);
-        // }else{
-        //     res.status(404).json({
-        //         message : "Aucun contenu"
-        //     })
-        // }
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id,
+            },
+          };
+        }),
+      };
+      // if(docs.length >=0){
+      res.status(200).json(response);
+      // }else{
+      //     res.status(404).json({
+      //         message : "Aucun contenu"
+      //     })
+      // }
     })
-    .catch( err => {
-        console.log(err)
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+//Ajoute un nouveau produit
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Created product successfull",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result.id,
+          },
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: error,
+      });
+    });
+});
+//Récupere un produit grace à un ID
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .select("name price _id")
+    .exec()
+    .then((doc) => {
+      console.log("From data base : ", doc);
+      if (doc) {
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://localhost/products",
+          },
+        });
+      } else {
+        res.status(404).json({ message: "L'id n'est pas valide" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+//Met à jour un produit grace à un ID
+router.patch("/:productId", (req, res, next) => {
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.updateOne({ _id: req.params.productId }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Produit mis à jour",
+        request: "http://localhost:3000/products/" + req.params.productId,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+//Supprime un produit grace à son ID
+router.delete("/:productId", (req, res, next) => {
+  Product.remove({
+    _id: req.params.productId,
+  })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Produit supprimé",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/products",
+          body: {
+            name: "String",
+            price: "Number",
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err),
         res.status(500).json({
-            "error" : err
+          error: err,
         });
     });
-})
-//Ajoute un nouveau produit
-router.post('/',(req, res, next)=>{
-    const product = new Product({
-        _id : new mongoose.Types.ObjectId(),
-        name : req.body.name,
-        price : req.body.price
-    })
-    product.save().then(result=>{
-        console.log(result)
-        res.status(200).json({
-            createdProduct : product
-        })
-    })
-    .catch(error=>{
-        console.log(error)
-        res.status(500).json({
-            error : error
-        })
-    })
-    
-})
-//Récupere un produit grace à un ID
-router.get('/:productId', (req, res, next)=>{
-    const id = req.params.productId;
-    const ObjectID = mongoose.Types.ObjectId;
-  
-        Product.findById(id)
-   .exec()
-   .then(doc => {
-       console.log("From data base : ",doc)
-       if(doc){
-        res.status(200).json(doc)
-       }else{
-           res.status(404).json({message : "L'id n'est pas valide"})
-       }
-       
-   })
-   .catch(err=>{
-       console.log(err);
-       res.status(500).json({error : err});
-    });
-  
-   
-})
-//Met à jour un produit grace à un ID
-router.patch('/:productId', (req, res, next)=>{
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    Product.updateOne({_id : req.params.productId},{$set: updateOps})
-    .exec()
-    .then(result=>{
-        console.log(result);
-        res.status(200).json(result);
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error : err
-        })
-    });
-
-})
-//Supprime un produit grace à son ID
-router.delete('/:productId', (req, res, next)=>{
-    Product.remove({
-        _id : req.params.productId
-    })
-    .exec()
-    .then(result =>{
-        res.status(200).json(result);
-    })
-    .catch(err=>{
-        console.log(err),
-        res.status(500).json({
-            error : err
-        })
-    })
-})
+});
 
 module.exports = router;
